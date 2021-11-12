dd64a6cd3ace05f81620e7f6f53672cad2af683a
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-12 14:48:30 +0100

    add basic print script

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 var app = new Vue(
     {
         el: '#myApp',
+        data: {
+            tasks: [
+                {
+                    text: 'fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'portare fuori la spazzatura',
+                    done: false
+                },
+                {
+                    text: 'bagnare i fiori',
+                    done: true
+                },
+                {
+                    text: 'pagare le bollette',
+                    done: false
+                },
+                {
+                    text: 'fare la lavatrice',
+                    done: false
+                }
+            ]
+        }
     }
 );
